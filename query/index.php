2eee85770f3e1f51b0b2cea68ed94af62df17eee
Simon David HERNANDEZ <mail@sdavidhernandez.com>
2017-06-17 19:26:20 +0200

    bni - secondary search

--- query/index.php
+++ query/index.php
@@ -38,21 +38,39 @@
     <div class="container mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-layout--no-drawer-button">
         <header class="header mdl-layout__header">
             <div class="mdl-layout__header-row">
-                <span class="mdl-layout-title"></span>
+                <span class="mdl-layout-title">
+                    <a href="/bni/">Bienvenue dans la Bibliothèque Numérique des Idées</a>
+                </span>
                 <div class="mdl-layout-spacer"></div>
                 <nav class="mdl-navigation">
-                    <div class="header-img-avatar">
-                        <div class="img-container">
-                            <img class="img-avatar" src="adam-smith.jpg" style="margin-left: -4vh;">
-                        </div>
-                    </div>
                 </nav>
             </div>
         </header>
+                    <!-- div class="header-img-avatar">
+                        <div class="img-container">
+                            <img class="img-avatar" src="adam-smith.jpg" style="margin-left: -4vh;">
+                        </div>
+                    </div-->
+
         <main class="content mdl-layout__content">
-            <h2 class="center">Immersion dans l'oeuvre d'Adam SMITH</h2>
             <section class="mdl-grid">
-                <div class="mdl-card mdl-card--expand mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-cell--12-col-desktop mdl-cell--12-col-tablet mdl-cell--12-col-phone">
+                <div class="mdl-cell--6-col"><h5>Immersion dans l'oeuvre d'Adam SMITH</h5></div>
+                <div class="mdl-cell--6-col">
+                    <form action="/bni/search/" class="search search--secondary" autocomplete="off">
+                        <div class="mdl-textfield mdl-js-textfield">
+                            <input id="query" class="mdl-textfield__input" type="text" 
+                                placeholder="Recherche" value="<?php echo $_REQUEST["query"];?>" >
+                        </div>
+                        <button id="search" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                            <label class="mdl-button mdl-js-button mdl-button--icon">
+                                <i class="material-icons">search</i>
+                            </label>
+                            Chercher
+                        </button>
+                    </form>
+                </div>
+
+                <div class="mdl-card mdl-card--expand mdl-cell mdl-cell--12-col mdl-cell--12-col-desktop mdl-cell--12-col-tablet mdl-cell--12-col-phone">
                     <div class="mdl-card__supporting-text mdl-cell mdl-cell--12-col ">
                             <div class="query-from">
                                 <span class="mdl-color-text--blue-grey-900">Résultats:</span> 
@@ -78,5 +96,6 @@
     <script src="js/jquery-1.10.2.js"></script>
     <script src="js/main.js"></script>
     <script src="js/solr.query.js"></script>
+    <script src="js/search.js"></script>
 </body>
 </html>
