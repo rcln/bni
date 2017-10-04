f04ec9e295eb6c095168376eb2031cdcb9fab75d
Simon David HERNANDEZ <mail@sdavidhernandez.com>
2017-10-04 22:30:25 +0200

    Web. Adding list of books for FH.

---
+++ js/list-works.js
@@ -0,0 +1,14 @@
+$(function(){
+    $.get(bnicfg.url_to_source_beta + bnicfg.url_get_works 
+            + " AND lang:fr AND type:primary_literature "
+            + " AND author:" + default_author,
+        function(data){
+            var works = data.grouped.file_name_hash.groups;
+            for(w in works){
+                var work = works[w].doclist.docs[0];
+                var title = work.title;
+                $("#oeuvres").append("<li>" + title + "</li>")
+            }
+        }
+    , "json");
+});
